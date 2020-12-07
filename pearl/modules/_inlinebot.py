55320187c827c4f76158caabfaf3cf0f588d1cbd
MrSammy <75217387+IntelligentSammy@users.noreply.github.com>
2020-12-07 20:14:29 +0530

    😬

--- pearl/modules/_inlinebot.py
+++ pearl/modules/_inlinebot.py
@@ -89,7 +89,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 "Menu Closed!!",
             )
         else:
-            reply_pop_up_alert = "Please get your own userbot from @pearlsupport "
+            reply_pop_up_alert = "Please get your own userbot from @BlackPearlsupport "
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
                
